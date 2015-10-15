050ad7c42edb97702919031268cbee6e9b2e77f1
Vítor Teixeira <vitor.teixeira@platformblack.com>
2015-10-15 17:19:41 +0100

    Some housecleaning

--- src/main/java/com/vframework/util/FileUtils.java
+++ src/main/java/com/vframework/util/FileUtils.java
@@ -1,5 +1,6 @@
 package com.vframework.util;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -11,14 +12,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
+import java.util.zip.CRC32;
 
 import javax.xml.bind.DatatypeConverter;
 
 public class FileUtils {
 	
-	public static String readFile(final String fileLocation) {
-		File file = new File(fileLocation);
+	public static String readFile(String filePath) {
+		File file = new File(filePath);
 		//Only supports files up to (2^31 - 1) bytes (2147483647)
 		byte[] byteArray = new byte[(int) file.length()];
 		
@@ -49,11 +50,11 @@ public class FileUtils {
 		return new String(byteArray, StandardCharsets.UTF_8);
 	}
 	
-	public static void writeFile(final InputStream inputStream, final String fileLocation) {
+	public static void writeFile(InputStream inputStream, String filePath) {
 		OutputStream outputStream = null;
 		
 		try {
-			File fileToUpload = new File(fileLocation);
+			File fileToUpload = new File(filePath);
 			outputStream = new FileOutputStream(fileToUpload);
 			
 			byte[] buffer = new byte[2048];
@@ -73,13 +74,13 @@ public class FileUtils {
 		}		
 	}
 	
-	public static void copyFile(final File sourceFile, final File destinationFile) {
+	public static void copyFile(File sourceFilePath, File destinationFilePath) {
 		InputStream inputStream = null;
 		OutputStream outputStream = null;
 		
 		try {
-			inputStream = new FileInputStream(sourceFile);
-			outputStream = new FileOutputStream(destinationFile);
+			inputStream = new FileInputStream(sourceFilePath);
+			outputStream = new FileOutputStream(destinationFilePath);
 			
 			byte[] buffer = new byte[2048];
 			int length = 0;
@@ -99,31 +100,123 @@ public class FileUtils {
 		}
 	}
 	
-	public static String readFileJava7(final String fileLocation) throws IOException {
-		return new String(Files.readAllBytes(Paths.get(fileLocation)), StandardCharsets.UTF_8);
+	public static String readFileJava7(String filePath) throws IOException {
+		return new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
 	}
 	
-	public static void writeFileJava7(final InputStream inputStream, final String fileLocation) throws IOException {
-		Files.copy(inputStream, Paths.get(fileLocation));
+	public static void writeFileJava7(InputStream inputStream, String filePath) throws IOException {
+		Files.copy(inputStream, Paths.get(filePath));
 	}
 	
-	public static void copyFileJava7(final File sourceFile, final File destinationFile) throws IOException {
-	    Files.copy(sourceFile.toPath(), destinationFile.toPath());
+	public static void copyFileJava7(File sourceFilePath, File destinationFilePath) throws IOException {
+	    Files.copy(sourceFilePath.toPath(), destinationFilePath.toPath());
 	}
 	
-	public static String getMD5HashJava7(final String fileLocation) throws IOException, NoSuchAlgorithmException {
+	public static String getMD5SumJava7(String filePath) throws NoSuchAlgorithmException, IOException {
 		MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-		messageDigest.update(Files.readAllBytes(Paths.get(fileLocation)));
-		
-		byte[] digest = messageDigest.digest();
-		return Arrays.toString(digest);
-	}
-	
-	public static String getMD5HashHexJava7(final String fileLocation) throws NoSuchAlgorithmException, IOException {
-		MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-		messageDigest.update(Files.readAllBytes(Paths.get(fileLocation)));
+		messageDigest.update(Files.readAllBytes(Paths.get(filePath)));
 		
 		byte[] digest = messageDigest.digest();
 		return DatatypeConverter.printHexBinary(digest).toUpperCase();
 	}
+	
+	public static String getSHA1SumJava7(String filePath) throws NoSuchAlgorithmException, IOException {
+		MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
+		messageDigest.update(Files.readAllBytes(Paths.get(filePath)));
+		
+		byte[] digest = messageDigest.digest();
+		return DatatypeConverter.printHexBinary(digest).toUpperCase();
+	}
+	
+	public static String getMD5Sum(InputStream inputStream) throws NoSuchAlgorithmException, IOException {
+		MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+		
+		byte[] buffer = new byte[2048];
+		int length = 0;
+		
+		while((length = inputStream.read(buffer)) > 0) {
+			messageDigest.update(buffer, 0, length);
+		}
+		
+		byte[] hashBytes = messageDigest.digest();
+		
+		StringBuffer hexStringBuffer = new StringBuffer();
+		for(int i = 0; i < hashBytes.length; i ++) {
+			hexStringBuffer.append(Integer.toString((hashBytes[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		
+		return hexStringBuffer.toString();
+	}
+	
+	public static boolean checkMD5Sum(InputStream inputStream, String sum) throws NoSuchAlgorithmException, IOException {
+		return getMD5Sum(inputStream).equals(sum);
+	}
+	
+	public static String getSHA1Sum(InputStream inputStream) throws NoSuchAlgorithmException, IOException {
+		MessageDigest messageDigest = MessageDigest.getInstance("SHA1");
+		
+		byte[] buffer = new byte[2048];
+		int length = 0;
+		
+		while((length = inputStream.read(buffer)) > 0) {
+			messageDigest.update(buffer, 0, length);
+		}
+		
+		byte[] hashBytes = messageDigest.digest();
+		
+		StringBuffer hexStringBuffer = new StringBuffer();
+		for(int i = 0; i < hashBytes.length; i ++) {
+			hexStringBuffer.append(Integer.toString((hashBytes[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		
+		return hexStringBuffer.toString();
+	}
+	
+	public static boolean checkSHA1Sum(InputStream inputStream, String sum) throws NoSuchAlgorithmException, IOException {
+		return getSHA1Sum(inputStream).equals(sum);
+	}
+	
+	public static long getCRC32(FileInputStream fileInputStream) throws IOException {
+		CRC32 crc = new CRC32();
+		
+		InputStream inputStream = new BufferedInputStream(fileInputStream);
+		int length = 0;
+		
+		while((length = inputStream.read()) > 0) {
+			crc.update(length);
+		}
+		
+		return crc.getValue();
+	}
+	
+	public static boolean checkCRC32(FileInputStream fileInputStream, long crc) throws IOException {
+		return getCRC32(fileInputStream) == crc;
+	}
+	
+	public static String getSHA256Sum(InputStream inputStream) throws NoSuchAlgorithmException, IOException {
+		MessageDigest messageDigest = MessageDigest.getInstance("SHA256");
+		
+		byte[] buffer = new byte[2048];
+		int length = 0;
+		
+		while((length = inputStream.read(buffer)) > 0) {
+			messageDigest.update(buffer, 0, length);
+		}
+		
+		byte[] hashBytes = messageDigest.digest();
+		
+		StringBuffer hexStringBuffer = new StringBuffer();
+		for(int i = 0; i < hashBytes.length; i ++) {
+			hexStringBuffer.append(Integer.toString((hashBytes[i] & 0xff) + 0x100, 16).substring(1));
+		}
+        
+        return hexStringBuffer.toString();
+    }
+	
+	public static boolean checkSHA256Sum(InputStream inputStream, String sum) throws NoSuchAlgorithmException, IOException {
+		return getSHA256Sum(inputStream).equals(sum);
+	}
+	
+	//TODO - cleanup this utility class (be consistent, either use BufferedInputStream/FileInputStream/InputStream)
+	//TODO - wrap everything around FrameworkException
 }
